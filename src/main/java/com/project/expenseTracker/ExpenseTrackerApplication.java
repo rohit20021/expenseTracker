97ffa7f4ff17ad9363d6f0ed9920eaef5cad23a5
Rohit Garg <rohitgarg20021@gmail.com>
2026-05-27 14:56:34 +0530

    main application commit

--- src/main/java/com/project/expenseTracker/ExpenseTrackerApplication.java
+++ src/main/java/com/project/expenseTracker/ExpenseTrackerApplication.java
@@ -2,8 +2,10 @@ package com.project.expenseTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
